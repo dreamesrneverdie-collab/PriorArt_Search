19d9aac14f5562e163a86defc1c4dcf6cfe7c2d4
thaodtp2416615 <thao.dtp2416615@sis.hust.edu.vn>
2025-09-05 15:52:36 +0700

    Remove search_web_for_keywords from imports

--- src/patent_search_agent/tools/__init__.py
+++ src/patent_search_agent/tools/__init__.py
@@ -1,12 +1,11 @@
 """External API tools for the Patent Search Agent."""
 
-from .tavily_search import search_web_for_keywords, search_web_for_summary
+from .tavily_search import search_web_for_summary
 from .google_patents import search_google_patents
 from .ipccat_api import get_ipc_classification
 from .patent_crawler import crawl_patent_details
 
 __all__ = [
-    "search_web_for_keywords",
     "search_web_for_summary", 
     "search_google_patents",
     "get_ipc_classification",
